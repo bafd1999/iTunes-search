87aa3155db6453f7ddaead43495ce9897c4b6742
Rick <bafd1999@yahoo.com>
2018-08-17 13:13:04 -0500

    init

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Toolbar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+  import Toolbar from '~/components/Toolbar.vue'
+  export default {
+    components: {
+      Toolbar
+    }
+  }
+</script>
